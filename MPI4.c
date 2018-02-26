e4958298bbef3c59739b8dbdbef6711c6ee65ea8
Sagar Shah <shah7sagar7@gmail.com>
2018-02-26 08:38:48 -0800

    Using only the routines that we have covered in the three examples in lectures (MPI_Init, MPI_Comm_Rank, MPI_Send, MPI_Recv, MPI_Barrier, MPI_Finalize), Write a program that determines how many PEs it is running on. It should perform like the following:

---
+++ MPI4.c
@@ -0,0 +1,29 @@
+#include <mpi.h>
+#include <stdio.h>
+
+int main(int argc, char** argv)  //main function//
+{
+    int tea, i, function, error; //initialization stage//
+	int my_PE_num, numbertoreceive; 
+
+	MPI_Status status;  
+    MPI_Init(&argc, &argv);
+	MPI_Errhandler_set(MPI_COMM_WORLD,MPI_ERRORS_RETURN); //to set mpi error handler to return error value//
+
+		i = 0;
+		tea=0;  //tea = temperary variable//
+		while(i!=70) //took a random value of 70 to check the error// 
+    	{	     
+        	tea+=1;
+        	//error=MPI_Send( &function, 1, MPI_INT, i, 10, MPI_COMM_WORLD);
+			error=MPI_Bcast(&function, 1, MPI_INT, i, MPI_COMM_WORLD); //to brodcast a function value//
+				if(error!=0) //to detect and print the error//				
+				{
+					printf("\nI am running on %d PEs\n",tea-1);
+					MPI_Abort(MPI_COMM_WORLD,error);//to stop the operation//
+				}
+			i+=1;
+   		}
+    MPI_Finalize();
+    return 0;
+}
